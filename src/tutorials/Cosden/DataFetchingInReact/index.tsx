762b8632ac191ed956d2b92798e0e3a08021e6df
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-04-22 08:26:08 +0300

    22.04.2024

---
+++ src/tutorials/Cosden/DataFetchingInReact/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/'
+
+
+
+export default function index() {
+    return (
+        <div>index</div>
+    )
+}
