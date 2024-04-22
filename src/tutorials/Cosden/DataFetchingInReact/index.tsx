ea7505f5c309d18af60dfb2184184a9252eed8a5
Aleksandr Marchenko <proshta@gmail.com>
2024-04-22 12:49:24 +0300

    Update index.tsx

--- src/tutorials/Cosden/DataFetchingInReact/index.tsx
+++ src/tutorials/Cosden/DataFetchingInReact/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useState } from 'react'
 
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com/'
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 interface Post {
     id: number
@@ -12,20 +12,87 @@ interface Post {
 
 
 
-export default function Demo() {
 
+
+
+
+
+export default function Demo() {
+    const [error, setError] = useState()
+    const [isLoading, setIsLoading] = useState(false)
     const [posts, setPosts] = useState<Post[]>([])
+    const [page, setPage] = useState(0)
+
+
+    const abortControllerRef = useRef<AbortController | null>(null)
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+
+            abortControllerRef.current?.abort()
+            abortControllerRef.current = new AbortController()
+
+
+            setIsLoading(true)
+            try {
+                const response = await fetch(`${BASE_URL}/posts?page=${page}`,
+                    { signal: abortControllerRef.current.signal }
+                );
+                const posts = (await response.json()) as Post[];
+                setPosts(posts)
+            } catch (error: any) {
+                
+                if (error.name === 'AbortError') {
+                    console.log('fetch aborted')
+                    return;
+                }
+
+                setError(error)
+            } finally {
+                setIsLoading(false)
+            }
+
+
+        }
+
+        fetchPosts()
+
+    }, [page])
+
+
+
+
+
+    if (error) {
+        return <div>Error: Please tyr again</div>
+    }
+
 
 
     return (
         <div>
             <h1 className="mb-4 text-2xl " >Data Fetching in React</h1>
-            <ul>
+            <button
+                className="bg-blue-500 text-white px-3 py-2 rounded-md"
+                onClick={() => setPage(prev => prev + 1)}
+            >
+                Next {page + 1}
+            </button>
 
-                {posts.map(
-                    (post) => { return <li key={post.id}>{post.title}</li> }
-                )}
-            </ul>
+
+
+            {isLoading ?
+
+                <div>Loading...</div>
+                :
+                <ul>
+                    {posts?.map(
+                        (post) => { return <li key={post.id}>{post.title}</li> }
+                    )}
+                </ul>
+            }
+
+
 
         </div>
     )
