39626b1874dafc1872b535076bc868775913a10e
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-05-08 06:43:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import { } from 'react'
 
 import './App.css'
-import ReactRouter from './tutorials/Cosden/9.ReactRouter/ReactRouter'
 
-// import Fetching from "./tutorials/Cosden/1.DataFetchingInReact/index.tsx"
-// import KeysAndList from "./tutorials/Cosden/2.KeysAndList/index.tsx"
-// import ReactQuery from "./tutorials/Cosden/3.ReactQuery/index.tsx"
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// import ReduxApp from './tutorials/Cosden/5.Redux/ReduxApp'
-// import ReactHookForm from './tutorials/Cosden/8.ReactHookForm/ReactHookForm'
-import ServerComponents from './tutorials/Cosden/10-1.ServerComponents/ServerComponents'
-
-
-
-
-// const queryClient = new QueryClient()
 
 
 
@@ -27,7 +13,7 @@ function App() {
     <>
 
       
-      <ServerComponents />
+      
 
     </>
   )
