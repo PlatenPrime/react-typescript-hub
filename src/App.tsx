be1b9ef9db54f9d1baa7d4f20e6be07e83e79278
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-05-27 07:57:49 +0300

    27.05.2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { } from 'react'
 
 import './App.css'
-import Index from './tutorials/DaveGray/Typescript/7.IndexSignatures/Index'
+import Index from './tutorials/DaveGray/Typescript/8.Generics/Index'
 
 
 
---
+++ src/tutorials/DaveGray/Typescript/8.Generics/Index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Index() {
+
+
+
+const stringEcho = (arg: string): string => arg
+// const echo = <T>(arg: T): T => arg
+
+
+    const isObject = <T extends object>(arg: T): boolean => {
+        return typeof arg === 'object' && !Array.isArray(arg) && arg !== null
+        }
+
+
+
+
+        // const isTrue = <T >(arg: T, is: boolean) => {
+        //     if(Array.isArray(arg) && arg.length === 0) {
+        //         return {arg, is: false}
+        //     }
+        //     if (isObject(arg) && Object.keys(arg as keyof T).length === 0) {
+        //         return {arg, is: false}
+        //     }
+        //     return {arg, is: !!arg}
+        //     }
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>Index Generics</div>
+    )
+}
