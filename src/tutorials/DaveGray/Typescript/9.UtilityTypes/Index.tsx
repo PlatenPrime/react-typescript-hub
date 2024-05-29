652b62388a9f10aeae98c396d4d476364326418b
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-05-29 07:28:15 +0300

    Update Index.tsx

--- src/tutorials/DaveGray/Typescript/9.UtilityTypes/Index.tsx
+++ src/tutorials/DaveGray/Typescript/9.UtilityTypes/Index.tsx
@@ -77,8 +77,98 @@ const finalGrades: Record<Students, LetterGrades> = {  Coda: "A", Prime: "B", Pl
 
 
 
+interface Grades {
+    assign1: number;
+    assign2: number;
+    assign3: number;
+}
 
 
+const gradeData: Record<Students, Grades> = {
+    Comsats: { assign1: 79, assign2: 0, assign3: 83 },
+    Coda: { assign1: 79, assign2: 0, assign3: 83 },
+    Prime: { assign1: 79, assign2: 0, assign3: 83 },
+    Platen: { assign1: 79, assign2: 0, assign3: 83 },
+    CodaPrime: { assign1: 79, assign2: 0, assign3: 83 },
+}
+
+
+
+
+// Pick and Omit
+
+
+type AssignPreview = Pick<Assignment, 'studentId' | 'title'>
+
+type AssignPreview2 = Omit<Assignment, 'grade' | 'verified'>
+
+
+const score: AssignPreview2 = { studentId: 'comsats', title: 'React' }
+const score2: AssignPreview = {studentId: 'comsats', title: 'React'}
+
+
+
+
+
+// Exclude and Extract
+
+
+type AssignResults = Exclude<Assignment, 'grade' | 'verified'>
+
+type AssignResults2 = Extract<Assignment, 'grade' | 'verified'>
+
+
+
+
+// NonNullable
+
+
+type AllPossibleGrades = "Dave" | "John" | null | undefined | "Comsats"
+
+type ReallyAllPossibleGrades = NonNullable<AllPossibleGrades>
+
+
+
+
+
+
+// ReturnType
+
+
+type newAssign = { title: string; points: number }
+
+type NewAssign = ReturnType<typeof newAssign>
+
+const createNewAssign = (title: string, points: number): NewAssign => {
+    return { title, points }
+}
+
+
+type NewMake = ReturnType<typeof makeSng>
+
+
+const makeSng = (arg1: string, arg2: number) => {
+    return { title: arg1, points: arg2, verified: true }    
+}
+
+const assign: NewMake = makeSng('React', 100)
+
+
+
+
+
+
+
+type AssignParams = Parameters<typeof makeSng>
+
+
+const assignParams : AssignParams = [ 'React', 100 ]
+
+
+
+// Awaited 
+
+type MyAwaited<T> = T extends Promise<infer U> ? U : never
 
 
     return (
