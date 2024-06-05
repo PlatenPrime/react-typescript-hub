0378dce99480bdba6f4a9bd2d14de1269cf7b7f4
Aleksandr Marchenko <proshta@gmail.com>
2024-06-05 08:49:49 +0300

    Update Index.tsx

--- src/tutorials/DaveGray/Typescript/14.ReactHooks/Index.tsx
+++ src/tutorials/DaveGray/Typescript/14.ReactHooks/Index.tsx
@@ -18,13 +18,23 @@ export default function Index() {
     const [count, setCount] = useState<number>(0)
     const [users, setUsers] = useState<User[] | null>(null)
 
+    console.log(users);
+    
+
 
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            .then(data => setUsers(data))
-    }, [])
+        console.log('mounting')
+        console.log(users);
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(res => res.json())
+        //     .then(data => setUsers(data))
+
+
+            return () => {
+                console.log('unmounting')
+            }
+    }, [users])
 
 
 
